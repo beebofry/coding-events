0d60771fd025fa60bf85fc7bad56077207b5be94
kimlee <kiim.n.lee@gmail.com>
2022-10-18 17:31:04 -0500

    completed model validation exercises

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,18 +1,39 @@
 package org.launchcode.codingevents.models;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.*;
+import java.util.Date;
 import java.util.Objects;
 
 public class Event {
 
     private int id;
     private static int nextId = 1;
+   @NotBlank(message = "Name is required.")
+   @Size(min = 3, max = 50, message = "Name must be between 3-50 characters.")
     private String name;
+   @NotBlank(message = "Please enter a location.")
+   @Size(max = 500, message = "Description too long!")
+   private String location;
+
+   @Size(max = 500, message = "Description too long!")
     private String description;
+   @NotBlank(message = "Email is required.")
+   @Email(message = "Invalid email. Try again.")
+   private String contactEmail;
+   private boolean mustRegister = true;
 
+   @Positive(message = "Invalid number of attendees. Must be one or more.")
+   private int numberOfAttendees;
 
-    public Event(String name, String description) {
+    public Event() {}
+    public Event(String name, String location, String description, String contactEmail, int numberOfAttendees) {
         this.name = name;
+        this.location = location;
         this.description = description;
+        this.contactEmail = contactEmail;
+        this.numberOfAttendees = numberOfAttendees;
         this.id = nextId;
         nextId++;
     }
@@ -35,6 +56,31 @@ public class Event {
 
     public int getId() {
         return id;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public int getNumberOfAttendees() {
+        return numberOfAttendees;
+    }
+
+    public void setNumberOfAttendees(int numberOfAttendees) {
+        this.numberOfAttendees = numberOfAttendees;
+    }
+
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
     }
 
     @Override
